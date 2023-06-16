a83121eb5a7d8028581a8a4e6b6c218b92c309df
NkaisengE <nkaisengle@gmail.com>
2023-06-16 14:43:32 -0700

    3-print

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,12 +11,12 @@ int upperCase = 'A';
 while (lowerCase <= 'z')
 {
 putchar(lowerCase);
-lowerCase +=1;
+lowerCase += 1;
 }
 while (upperCase <= 'Z')
 {
 putchar(upperCase);
-upperCase +=1;
+upperCase += 1;
 }
 putchar('\n');
 return (0);
